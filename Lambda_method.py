da9508ed78683782ddb9f29f899e81760312d3d7
DiegING <diego.padilla4694@alumnos.udg.mx>
2026-03-04 00:14:39 -0600

    Confirmando reubicacion de Lambda_method y limpieza

--- Lambda_method.py
+++
@@ -1,8 +0,0 @@
-list_1 = [x for x in range(5)]                   
-print(list_1)
-list_2 = list(map(lambda x: 2 ** x, list_1))        #The map() method is used here
-print(list_2)
-
-for x in map(lambda x: x * x, list_2):
-    print(x, end=' ')
-print()
