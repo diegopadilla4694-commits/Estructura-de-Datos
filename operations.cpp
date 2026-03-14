8f426acf8308ce16a03d0c8cdfbf4238e0666c37
DiegING <diego.padilla4694@alumnos.udg.mx>
2026-03-14 17:29:56 -0600

    Getting Started in C++

---
+++ operations.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+
+int main()
+{
+    int a=10;
+    int b=20;
+    int c=34;
+    int d=45;
+    std::cout<<"sum of a and b and c and d is "<<a+b+c+d<<std::endl;
+
+    return 0;
+}
